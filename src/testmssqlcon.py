06dafd64186826e6d36e9e31067ff6f06d6505b8
TanabutT <t_tanabut@hotmail.com>
2025-12-03 13:18:47 +0700

    conn success

--- src/testmssqlcon.py
+++ src/testmssqlcon.py
@@ -1,12 +1,18 @@
-from mssql_client import *
+# connection to mssql server with python and this conn string 
+"""
+Connects to a SQL database using mssql-python
+"""
 
-mssql_client = MSSQLClient()._connect()
-#Execute a small test query
-rows = MSSQLClient()._execute_query("SELECT * FROM [dbo].[test]")
+from os import getenv 
+from dotenv import load_dotenv 
+import pyodbc # Make sure you import pyodbc
 
-if rows is None:
-    print("MSSQL test query returned no rows")
-    
+load_dotenv()  # take environment variables from .env.
 
-print("MSSQL connectivity test succeeded")
-    
+# The entire connection string is read from the environment variable as one string
+connection_string = getenv("SQL_CONNECTION_STRING") 
+
+# Pass the single string to the connect function
+conn = pyodbc.connect(connection_string) 
+print("connection successful")
+# ... rest of your code ...
